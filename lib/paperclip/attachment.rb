1f3bfdbc7a84ef4ee6880ae493e2972a098c381f
Henrik N <henrik@nyh.se>
2008-11-10 09:19:48 +0100

    Log debug message when getting dimensions fails.

--- lib/paperclip/attachment.rb
+++ lib/paperclip/attachment.rb
@@ -85,6 +85,7 @@ module Paperclip
         dimensions = Paperclip::Geometry.from_file(uploaded_file)
         width, height = dimensions.width, dimensions.height
       rescue  # don't choke on bad images
+        logger.debug("[paperclip] Couldn't get dimensions for #{name}")
       end if image?
       instance_write(:width,           width, optional=true)
       instance_write(:height,          height, optional=true)
